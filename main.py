e4e07341a20bbab67bca589b783de5f5a91f2fa7
Sam Johnson <pixelspammy@gmail.com>
2019-10-24 17:02:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 import discord
+from discord.ext import commands
+
+client = discord.Client()
+BOT_TOKEN = "NjM3MDYzMTMwNDkyNTY3NTUy.XbItYA.2Yzh7FtyxMPlsW2ieb-ppyF5UfI"
+bot = commands.Bot(command_prefix='@')
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@bot.command
+async def test(ctx):
+    pass
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('yeee boiii! YEET')
+
+
+# Bot token
+client.run(BOT_TOKEN)
